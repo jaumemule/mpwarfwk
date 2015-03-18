6606d16776db3cd299c5dc796d745866a6cd68a8
jaumemule <jaumemule@gmail.com>
2015-03-18 16:54:10 +0100

    response

---
+++ src/Mpwarfwk/Component/JsonResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mpwarfwk\Component;
+
+class JsonResponse extends Response
+{
+    public function send()
+    {
+        if ($this->status != 200)
+        {
+            // Add needed header. For example 404.
+            header("HTTP/1.0 404 Not Found");
+        }
+
+        header('Content-Type: application/json');
+
+        if (!is_array($this->content))
+        {
+            $this->content = array($this->content);
+        }
+
+        echo json_encode($this->content);
+    }
+}
