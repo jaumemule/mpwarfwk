b3dd48682f7adaf737edbd78c905252d74371cec
jaumemule <jaumemule@gmail.com>
2015-05-30 18:23:47 +0200

    ORM

--- src/Mpwarfwk/Component/Model/PdoDatabase.php
+++ src/Mpwarfwk/Component/Model/PdoDatabase.php
@@ -7,6 +7,17 @@ class PdoDatabase{
 
     private $database;
 
+    //SIMPLE MOST USED DATA
+    private $conversion_data = array(
+            'integer'           => PDO::PARAM_INT ,
+            'string'            => PDO::PARAM_STR ,
+            'input_output'      => PDO::PARAM_INPUT_OUTPUT ,
+            'bool'              => PDO::PARAM_BOOL  ,
+            'null'              => PDO::PARAM_NULL  ,
+            'lob'               => PDO::PARAM_LOB   ,
+            'number'            => PDO::FETCH_NUM    ,
+        );
+
     public function __construct(DBconnection $DBconnection) {
 
         $this->database = new PDO('mysql:dbname='.DBconnection::$database.';host='.DBconnection::$host.'', ''.DBconnection::$username.'', ''.DBconnection::$password.'');
@@ -34,6 +45,37 @@ class PdoDatabase{
         return $statement->fetchAll();
     }
 
+    public function selectFromTableBindData($query, $data) {
+
+        $statement = $this->database->prepare($query);
+        if($data != NULL){
+            foreach ($data as $key => $actualValue) {
+                $statement->bindParam(":$key", $actualValue[$key], $this->conversion_data[$actualValue['type']], $actualValue['lenght']);
+            }
+        }
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+
+    public function insertInTableBindData($query, $data) {
+
+        $statement = $this->database->prepare($query);
+        foreach ($data as $key => $actualValue) {
+                $statement->bindParam(":$key", $actualValue[$key], $this->conversion_data[$actualValue['type']], $actualValue['lenght']);
+        }
+        return $statement->execute();
+    }
+
+
+    public function updateTableBindData($query, $data) {
+        
+        $statement = $this->database->prepare($query);
+        foreach ($data as $key => $actualValue) {
+            $statement->bindParam(":$key", $actualValue[$key], $this->conversion_data[$actualValue['type']], $actualValue['lenght']);
+        }
+        return $statement->execute();
+    }  
+
 
     public function insertInTable($query, $data) {
 
@@ -50,6 +92,12 @@ class PdoDatabase{
         return $statement->execute();
     }
 
+    public function deleteFromFreeQuery($query) {
+
+        $statement = $this->database->prepare($query);
+        return $statement->execute();
+    }
+
     public function updateTable($query, $data) {
         
         $statement = $this->database->prepare($query);
@@ -58,4 +106,9 @@ class PdoDatabase{
         }
         return $statement->execute();
     }  
+
+    public function lastInsertID()
+    {
+        return $this->database->lastInsertId();
+    }
 }
